7337061372a56da4922d487532c3b3953f0c2063
ShimaaHamdi <shaimaahamdi023@gmail.com>
2022-07-06 13:49:22 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+// To define a variable in JavaScript, you use var keyword. For example:
+
+/*
+var x = 30;
+var y = 40;
+var z = x + y;
+document.write(z);
+*/
+
+//ES6 added a new way to declare a variable with the let keyword:
+/*
+let a = 10;
+let b = 4;
+let c = a + b;
+document.write(c);
+*/
+
+//To declare a function, you use the function keyword. The following example defines a function that calculates the sum of two arguments:
+/*
+function add(a, b) {
+  return a + b;
+}
+
+// To call the add() function, you use the following syntax:
+let a = 10;
+let b = 10;
+let result = add(a, b);
+document.write(result);
+*/
+
+//JavaScript provides you with condition statements such as if-else and switch statements. For example:
+/*
+let a = 20;
+let b = 30;
+function divide(a, b) {
+  if (b == 0) {
+    throw "Division By Zero ";
+  }
+  return a / b;
+}
+*/
+//The following JavaScript code doesn’t use whitespace:
+
+/*
+let formatted = true;
+if (formatted) {
+  console.log("The code is easy to read");
+}
+*/
+/*
+let formatted = true;
+
+if (formatted) {
+  console.log("The code is easy to read");
+}
+if (window.localStorage) {
+  console.log("The local storage is supported");
+}
+*/
+
+//For example, The following declares the message variable and initializes it with a literal string "Hello"
+let message;
+message = "Hello";
+
+//To declare and initialize a variable at the same time, you use the following syntax:
